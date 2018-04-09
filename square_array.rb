6daae7c34ed3fe9eacf2a7c2fb951a5277c6f2f0
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-09 00:00:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,12 @@ def square_array(array)
   new_array = []
   array.each do |number|
     new_array << number ** 2
-  end 
-  new_array 
+  end
+  new_array
 end
+
+def squar_array_with_collect(array) 
+  array.collect do |number|
+    number ** 2 
+  end 
+end 
